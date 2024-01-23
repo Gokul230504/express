ea1630f22943ff3659265f6dbea5dabcc05bf083
Gokul230504 <gokulrajl.21ece@kongu.edu>
2024-01-23 16:26:37 +0530

    my first jsx commit

---
+++ hi.js
@@ -0,0 +1,4 @@
+const head1= <h1>Gokul</h1>
+console.log(head1)//JSX = JS+XML cant executed in JAVASCRIPT FILE
+/*react will take the above code as below*/
+const head =React.createElement("h1",null,"Helloworld!");
